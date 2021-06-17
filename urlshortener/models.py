e1940d8273e9ae49ce66b3227b22fafcd0391c25
AndreaOrlando23 <64850221+AndreaOrlando23@users.noreply.github.com>
2021-06-17 23:13:43 +0200

    [FIX] fix model and makemigrations/migrate

--- urlshortener/models.py
+++ urlshortener/models.py
@@ -18,20 +18,26 @@ class Shortener(models.Model):
 	times_followed 	= models.PositiveIntegerField(default=0)   
 	long_url 		= models.URLField()
 	short_url 		= models.CharField(max_length=15, unique=True, blank=True)
-			
+
 
 	class Meta:
 
 		ordering = ["-created"]
 
-		def __str__(self):
+	def __str__(self):
 			return f"{self.long_url} to {self.short_url}"
+	
 
-
-		def save(self, *args, **kwargs):
+	def save(self, *args, **kwargs):
 			
 			if not self.short_url:
 				self.short_url = create_shortened_url(self)
 			super().save(*args, **kwargs)
+
+	
+		
+
+
+		
 		
 
